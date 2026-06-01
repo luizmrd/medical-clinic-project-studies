bb1c0406028e20a3b78fc7380a8876256ffdda1a
Luiz Miranda <lipesilvamiranda@hotmail.com>
2026-06-01 20:47:26 -0300

    Init project

---
+++ src/test/java/com/luizmrd/medical_clinic_project/MedicalClinicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizmrd.medical_clinic_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalClinicProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
